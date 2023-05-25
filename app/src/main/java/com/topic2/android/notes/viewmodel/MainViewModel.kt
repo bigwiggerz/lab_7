97a7a98cfec898bcea5597c25fe1d8c9b693d5ba
bigwiggerz <bigwiggeroffical@gmail.com>
2023-05-25 22:47:22 +0300

    change MainViewModel

--- app/src/main/java/com/topic2/android/notes/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/topic2/android/notes/viewmodel/MainViewModel.kt
@@ -1,6 +1,11 @@
 package com.topic2.android.notes.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.topic2.android.notes.domain.model.NoteModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import com.topic2.android.notes.data.repository.Repository
 
 /**
@@ -9,5 +14,24 @@ import com.topic2.android.notes.data.repository.Repository
  * Эта модель просмотра используется для всех экранов.
  */
 class MainViewModel(private val repository: Repository) : ViewModel() {
+    val notesNotInTrash: LiveData<List<NoteModel>> by lazy {
+        repository.getAllNotesNotInTrash()
+    }
+}
+fun onCreateNewNoteClick() {
 
+}
+
+fun onNoteClick(note: NoteModel) {
+
+}
+
+fun onNoteCheckedChange(
+    note: NoteModel
+) {
+    viewModelScope.launch(
+        Dispatchers.Default
+    ) {
+        repository.insertNote(note)
+    }
 }
