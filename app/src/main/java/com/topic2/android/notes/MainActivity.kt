db65ca2159e2adf31ea2739e8ef8ab586aca57af
bigwiggerz <bigwiggeroffical@gmail.com>
2023-05-25 14:50:54 +0300

    edit MainActivity

--- app/src/main/java/com/topic2/android/notes/MainActivity.kt
+++ app/src/main/java/com/topic2/android/notes/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.topic2.android.notes
 
 import android.os.Bundle
+import android.annotation.SuppressLint
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.rememberCoroutineScope
+import com.topic2.android.notes.routing.Screen
+import com.topic2.android.notes.theme.NotesTheme
+import com.topic2.android.notes.ui.components.AppDrawer
+import com.topic2.android.notes.ui.components.Note
 import com.topic2.android.notes.viewmodel.MainViewModel
 import com.topic2.android.notes.viewmodel.MainViewModelFactory
+import kotlinx.coroutines.launch
 
 /**
  * Main activity приложения.
@@ -18,6 +28,8 @@ class   MainActivity : AppCompatActivity() {
       (application as NotesApplication).dependencyInjector.repository
     )
   })
+
+  @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
