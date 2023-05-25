e25150558e378a7a489846d4aa3f6155e3d9fae8
bigwiggerz <bigwiggeroffical@gmail.com>
2023-05-25 22:14:34 +0300

    add cikirPicker func

--- app/src/main/java/com/topic2/android/notes/ui/components/screens/SaveNoteScreen.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/screens/SaveNoteScreen.kt
@@ -8,12 +8,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.topic2.android.notes.ui.components.NoteColor
+import androidx.compose.ui.text.font.FontWeight
 import com.topic2.android.notes.ui.fromHex
 import com.topic2.android.notes.domain.model.ColorModel
 
@@ -52,5 +55,48 @@ fun ColorItem(
 @Composable
 fun ColorItemPreview(){
     ColorItem(ColorModel.DEFAULT){
+    }
+}
+
+@Composable
+private fun ColorPicker(
+    color: List<ColorModel>,
+    onColorSelect: (ColorModel) -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(
+            text = "Color picker",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(8.dp)
+        )
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            items(
+                color.size
+            ) { itemIndex ->
+                val color = color[itemIndex]
+                ColorItem(
+                    color = color,
+                    onColorSelect = onColorSelect
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ColorPickerPreview(){
+    ColorPicker(
+        color = listOf(
+            ColorModel.DEFAULT,
+            ColorModel.DEFAULT,
+            ColorModel.DEFAULT
+        )
+    ){
     }
 }
